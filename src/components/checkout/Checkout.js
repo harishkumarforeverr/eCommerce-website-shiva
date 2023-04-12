a307e8167dc95d8e802281b2002592c8ccb84d36
harishkumarforever <harishking809@gmail.com>
2023-04-12 09:37:56 +0530

    new changes added

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -11,24 +11,24 @@ import Shipping from "./Shipping";
 import CreditCards from "./CreditCards";
 import CartSmall from "../cart/CartSmall";
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   paper: {
     padding: 12 * 2,
     color: theme.palette.text.secondary,
-    marginBottom: "20px"
+    marginBottom: "20px",
   },
   inputInfo: {
     fontSize: "16px",
     marginTop: "20px",
-    color: "black"
+    color: "black",
   },
   labelFocus: {
     color: "#1d1d1d",
-    borderColor: "#1d1d1d"
-  }
+    borderColor: "#1d1d1d",
+  },
 });
 
 class Checkout extends Component {
@@ -38,7 +38,7 @@ class Checkout extends Component {
       pane: 0,
       items: [],
       email: "",
-      address: {}
+      address: {},
     };
   }
   componentDidMount() {
@@ -46,19 +46,24 @@ class Checkout extends Component {
     const items = JSON.parse(localStorage.getItem(slug));
     this.setState({ items: items ? items : [] });
   }
-  changePane = pane => {
+  changePane = (pane) => {
     this.setState({ pane });
   };
   handleChange = (name, value) => {
-    this.setState({ [name]: value });
+    // this.setState({ [name]: value });
+    this.setState((prev) => ({
+      ...prev,
+      [name]: value ,
+      error: false,
+    }));
   };
-  createOrder = address => {
+  createOrder = (address) => {
     const { items, email } = this.state;
 
-    const itemSKUS = items.map(i => ({
+    const itemSKUS = items.map((i) => ({
       type: "sku",
       parent: i.sku_id,
-      quantity: +i.quantity
+      quantity: +i.quantity,
     }));
 
     let metadata = { status: "Ordered" };
@@ -77,24 +82,24 @@ class Checkout extends Component {
           city: address.locality,
           state: address.region,
           country: "US",
-          postal_code: address.postalCode
-        }
+          postal_code: address.postalCode,
+        },
       },
-      email: email
+      email: email,
     };
 
     fetch("/order/create", {
       method: "POST",
       headers: new Headers({ "content-type": "application/json" }),
-      body: JSON.stringify(postBody)
+      body: JSON.stringify(postBody),
     })
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         this.setState({ order_id: json.id });
       });
   };
 
-  setToken = token => {
+  setToken = (token) => {
     if (!this.state.order_id) {
       this.setState({ error: true });
       return;
@@ -104,14 +109,14 @@ class Checkout extends Component {
       headers: new Headers({ "content-type": "application/json" }),
       body: JSON.stringify({
         id: this.state.order_id,
-        source: token
-      })
+        source: token,
+      }),
     })
-      .then(response => response.json())
-      .then(order => {
+      .then((response) => response.json())
+      .then((order) => {
         this.props.history.push({
           pathname: "/confirm",
-          state: { order }
+          state: { order },
         });
       });
   };
@@ -138,93 +143,122 @@ class Checkout extends Component {
     }
     return (
       <PageWrapper>
-        {//Render Only if user has something in cart
-        this.state.items.length > 0 && (
-          <Grid
-            container
-            className={classes.root}
-            spacing={10}
-            direction={"row-reverse"}
-          >
-            <Grid item md={4} xs={12}>
-              <Paper className={classes.paper}>
-                <CartSmall items={this.state.items} config={config} />
-              </Paper>
-            </Grid>
-            <Grid item md={8} xs={12}>
-              <Paper className={classes.paper}>
-                <CheckoutHeader
-                  text={"Your Email"}
-                  classes={classes.heading}
-                  pane={0}
-                  currentPane={pane}
-                  changePane={() => this.changePane(0)}
-                />
-                {pane === 0 ? (
-                  <Email
-                    email={this.state.email}
-                    handleChange={this.handleChange}
+        {
+          //Render Only if user has something in cart
+          this.state.items.length > 0 && (
+            <Grid
+              container
+              className={classes.root}
+              spacing={10}
+              direction={"row-reverse"}
+            >
+              <Grid item md={4} xs={12}>
+                <Paper className={classes.paper}>
+                  <CartSmall items={this.state.items} config={config} />
+                </Paper>
+              </Grid>
+              <Grid item md={8} xs={12}>
+                <Paper className={classes.paper}>
+                  <CheckoutHeader
+                    text={"Your Email"}
+                    classes={classes.heading}
+                    pane={0}
+                    currentPane={pane}
+                    changePane={() => this.changePane(0)}
+                  />
+                  {pane === 0 ? (
+                    <span>
+                      <Email
+                        email={this.state.email}
+                        handleChange={this.handleChange}
+                        changePane={() => {
+                          var regexEmail =
+                            /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+                          if (regexEmail.test(this.state.email)) {
+                            this.changePane(1);
+                          } else {
+                            this.setState((prev) => ({
+                              ...prev,
+                              error: true,
+                            }));
+                          }
+                        }}
+                      />
+                      {this.state.error && (
+                        <span
+                          style={{
+                            color: "red",
+                            fontWeight: 600,
+                            marginTop: "7px",
+                          }}
+                        >
+                          {" "}
+                          Invalid email
+                        </span>
+                      )}
+                    </span>
+                  ) : (
+                    <div className={classes.inputInfo}>{this.state.email}</div>
+                  )}
+                </Paper>
+                <Paper className={classes.paper}>
+                  <CheckoutHeader
+                    text={"Shipping Address"}
+                    classes={classes.heading}
+                    pane={1}
+                    currentPane={pane}
                     changePane={() => this.changePane(1)}
                   />
-                ) : (
-                  <div className={classes.inputInfo}>{this.state.email}</div>
-                )}
-              </Paper>
-              <Paper className={classes.paper}>
-                <CheckoutHeader
-                  text={"Shipping Address"}
-                  classes={classes.heading}
-                  pane={1}
-                  currentPane={pane}
-                  changePane={() => this.changePane(1)}
-                />
-                {pane === 1 ? (
-                  <Shipping
-                    address={this.state.address}
-                    handleChange={this.handleChange}
-                    createOrder={this.createOrder}
+                  {pane === 1 ? (
+                    <Shipping
+                      address={this.state.address}
+                      handleChange={this.handleChange}
+                      createOrder={this.createOrder}
+                      changePane={() => this.changePane(2)}
+                    />
+                  ) : (
+                    displayAddress
+                  )}
+                </Paper>
+                <Paper className={classes.paper}>
+                  <CheckoutHeader
+                    text={"Payment"}
+                    classes={classes.heading}
+                    pane={2}
+                    currentPane={pane}
                     changePane={() => this.changePane(2)}
                   />
-                ) : (
-                  displayAddress
-                )}
-              </Paper>
-              <Paper className={classes.paper}>
-                <CheckoutHeader
-                  text={"Payment"}
-                  classes={classes.heading}
-                  pane={2}
-                  currentPane={pane}
-                  changePane={() => this.changePane(2)}
-                />
-                {this.state.error && (
-                  <p style={{ color: "#f40" }}>
-                    Sorry, an error has occurred. Please refresh the page and
-                    try again.
-                  </p>
-                )}
-                {!this.state.error && pane === 2 && (
-                  <CreditCards setToken={this.setToken} />
-                )}
+                  {this.state.error && (
+                    <p style={{ color: "#f40" }}>
+                      Sorry, an error has occurred. Please refresh the page and
+                      try again.
+                    </p>
+                  )}
+                  {!this.state.error && pane === 2 && (
+                    <CreditCards setToken={this.setToken} />
+                  )}
+                </Paper>
+              </Grid>
+            </Grid>
+          )
+        }
+        {
+          /*Don't allow the user to access the checkout page if they don't 
+        have anything in their cart*/
+          this.state.items.length === 0 && (
+            <Grid>
+              <Paper style={{ minHeight: "500px" }}>
+                <h2 style={{ padding: "40px 0 0 40px", fontWeight: 600 }}>
+                  Checkout
+                </h2>
+                <p style={{ paddingLeft: "40px" }}>
+                  Hmmmm, probably don't need to checkout if you don't have
+                  anything in your cart.
+                </p>
               </Paper>
             </Grid>
-          </Grid>
-        )}
-        {/*Don't allow the user to access the checkout page if they don't 
-        have anything in their cart*/
-        this.state.items.length === 0 && (
-          <Grid>
-            <Paper style={{ minHeight: "500px" }}>
-              <h2 style={{ padding: "40px 0 0 40px", fontWeight: 600 }}>
-                Checkout
-              </h2>
-              <p style={{ paddingLeft: "40px" }}>
-                Hmmmm, probably don't need to checkout if you don't have
-                anything in your cart.
-              </p>
-            </Paper>
-          </Grid>
-        )}
+          )
+        }
       </PageWrapper>
     );
   }
